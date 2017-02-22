6276cac70aa21cdb2c4c2599396993e94a4aa07b
helloworld2019 <969507437@qq.com>
2017-02-22 21:13:26 +0800

    Add files via upload

---
+++ practices/c/level1/p05_allPrimes/p05_allprimes.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<time.h>
+
+int main(  )
+{
+	int number;
+	printf("1 ");
+	printf("3 ");
+	printf("5 ");
+	printf("7 ");
+	for(number=11;number<=1000;number++){
+		int number1=number/2;
+		int k;
+		int flag=0;
+		for(k=2;k<=number1;k++){
+			if(number%k==0){
+				flag=0;
+				break;
+			}
+			flag=1;
+		}
+		if(flag==1)printf("%d ",number);
+	}
+	printf("\n%d ",clock());
+	return 0;
+
+
+}
