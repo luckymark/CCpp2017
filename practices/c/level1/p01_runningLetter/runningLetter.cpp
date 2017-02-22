63ea4c6a232d740e2165631f93be214447a439d8
ReChinX <cyuunichin@gmail.com>
2017-02-22 23:32:40 +0800

    完成了runningLetter

---
+++ practices/c/level1/p01_runningLetter/runningLetter.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <cstdlib>
+#include <windows.h>
+#define WIDTH 89
+#define SPACE 50
+char s[10000];
+int k=0,f=1;
+int main()
+{
+    for(int i=0;i<=WIDTH;i++)s[i]=' ';
+    while(1)
+    {
+        s[k]='R';
+        printf("%s",s);
+        s[k]=' ';
+        if(f>0)k++;else k--;
+        Sleep(SPACE);
+        system("cls");
+        if(k==WIDTH||k==0)f=-f;
+    }
+    return 0;
+}
