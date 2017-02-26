6063c04f4110bd8bc1566e82c37e0aa3a4b4a8c2
hideindark <s458314902@outlook.com>
2017-02-27 00:01:42 +0800

    new p01

--- practices/c/level1/p01_runningLetter/runningletter.c
+++ practices/c/level1/p01_runningLetter/runningletter.c
@@ -2,23 +2,17 @@
 #include<windows.h>
 int main()
 {
-	int n,i,j,k,speed;
+	int n,i,j,k=1,speed;
 	char ch='R';
 	scanf("%d %d",&n,&speed);
-	for(i=0;i<n;i++)
+	for(i=0;i<n;i+=k)
 	{
 		for(j=0;j<i;j++)putchar(' ');
 		putchar(ch);
 		Sleep(speed); 
+		if(k==-1&&i==0)break;
 		system("cls");
-	}
-	for(;i>=0;i--)
-	{
-		system("cls");
-		for(j=0;j<i;j++)putchar(' ');
-		putchar(ch);
-		Sleep(speed);
-		
+		if(i==n-1)k=-1;
 	}
 	return 0;
 }
