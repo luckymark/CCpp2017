870760cb788c087b5691cecf3d616fe9280d9fb8
= <=>
2017-02-28 12:09:33 +0800

    修改问题

--- practices/c/level1/p02_isPrime/isPrime.c
+++ practices/c/level1/p02_isPrime/isPrime.c
@@ -7,16 +7,16 @@ int main(void)//a为素数则输出1，a非素数则输出0
 	scanf("%d",&a);
 	if(a>1)
 	{
-	for(i=2;i<a;i++)//加入限制a大于1否则会出现1是素数的bug 
-	{
-		if(a%i==0)
-		{
-			printf("不是素数"); 
-		    return 0;
-		} 
-	}
-	printf("是素数"); 
-	return 0;
+		for(i=2;i<a;i++)//加入限制a大于1否则会出现1是素数的bug 
+			{
+				if(a%i==0)
+				{
+					printf("不是素数"); 
+				    return 0;
+				} 
+			}
+		printf("是素数"); 
+		return 0;
     }
     else
     {
--- practices/c/level1/p05_allPrimes/1000以下素数.c
+++ practices/c/level1/p05_allPrimes/1000以下素数.c
@@ -1,19 +1,10 @@
 #include<stdio.h>
-int i;
-int sushu(int a)//a为素数则输出1，a非素数则输出0 
-{
-	for(i=2;i<a;i++)//加入限制a大于1否则会出现1是素数的bug 
-	{
-		if(a%i==0)return 0;
-	} 
-	return 1;
-}
+int sushu(int a);//a为素数则输出1，a非素数则输出0 
 int main(void)
 {
-	int j;
 	int k=0;
-	int sus[1000];
-	for(j=2;j<1000;j++)//将100下素数放入sus中 
+	int sus[100];
+	for(int j=2;j<1000;j++)//将100下素数放入sus中 
 	{
 		if(sushu(j))//如果j是素数则放入sus中 
 		{
@@ -21,9 +12,15 @@ int main(void)
 			k++;
 		} 
 	}
-	k--;
-	int s;
-	for(s=0;s<=k;s++)       
-    printf("第%d个素数,值%d,共%d\n",s+1,sus[s],k+1);//检验一下素数正确性
-    return 0;
+	for(int s=0;s<k;s++)       
+    printf("第%d个素数,值%d,共%d\n",s+1,sus[s],k);输出素数 
+    return 0; 
+}
+int sushu(int a)
+{
+	for(int i=2;i<a;i++)//加入限制a大于1否则会出现1是素数的bug 
+	{
+		if(a%i==0)return 0;
+	} 
+	return 1;
 }
