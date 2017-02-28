885ac152970c4b000aebed08f6b5f9f149414b2e
xiaoyuann <602632139@qq.com>
2017-02-28 09:02:42 +0800

    level 0

---
+++ practices/c/level0/奇偶正负.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int n = 0;
+    printf("Please input n:");
+    scanf("%d",&n);
+
+    if (n>0)
+        printf("%d is positive number\n",n);
+    else if (n<0)
+        printf("%d is negative number\n",n);
+    else
+        printf("%d is zero\n",n);
+
+    if (0 == n % 2)
+        printf("%d is a even\n",n);
+    else
+        printf("%d is a obb\n",n);
+
+    return 0;
+}
