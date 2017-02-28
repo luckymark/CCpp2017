ebf30a2f9dd1a489fe5f0cc2f3d6f34bed1f0b44
lxambulance <751795314@qq.com>
2017-02-28 22:52:46 +0800

    F**K linkedlist

--- homeworklevel1/p11_linkedList.c
+++ homeworklevel1/p11_linkedList.c
@@ -11,21 +11,22 @@ inline int read() {
 }
 
 const int MAXN=200005;
-int n,m;
+int n,m,l;
 struct node{
         int x;
         node* next;
 	node(int xx=0):x(xx){}
-}stack[MAXN];
+};
 int main(){
 	printf("Please input the total number of the link list you want to create!\n");
 	n=read();
-	node* head=NULL,tail=NULL;
+	node *head=NULL,*tail=NULL;
 	for (int i=1;i<=n;++i) {
 		int k=read();
 		node* p=new node(k);
+		p->next=NULL;
 		if (head==NULL) head=p;
-		if (tail!=NULL) tail.next=p;
+		if (tail!=NULL) tail->next=p;
 		tail=p;
 	}
 	printf("Thanks\nPlease input the operation number\n");
@@ -35,35 +36,57 @@ int main(){
 	printf("4. 在该链表中查找第一个值为k的节点，如果找到则返回该节点的序号，否则返回-1\n");
 	printf("5. 查找下一个值为k的节点，返回值同上\n");
 	printf("6. 结束程序\n");
-	node* q;
-	while () {
+	node *q,*p,*stack[MAXN];
+	while (1) {
 		int k=read();
 		switch (k) {
 			case 1:{
-				node* p=new node(read());
+				p=new node(read());
+				p->next=NULL;
 				if (head==NULL) head=p;
-				if (tail!=NULL) tail.next=p;
+				if (tail!=NULL) tail->next=p;
 				tail=p; n++;
 				break;
 			}
 			case 2:{
-				for (node* p=head;p!=NULL;p=(*p)->next) printf("%d ",(*p)->x);
-				printf("\n%d\n",n);
+				for (p=head;p!=NULL;p=p->next) printf("%d ",p->x);
+				printf("\ntot=%d\n",n);
 				break;
 			}
 			case 3:{
-				
+				for (p=head,m=0;p!=NULL;p=p->next) stack[++m]=p;
+				tail=head=NULL;
+				for (int i=m;i;--i) {
+					p=stack[i];
+					if (head==NULL) head=p;
+					if (tail!=NULL) tail->next=p;
+					tail=p;
+				}
+				tail->next=NULL;
+				for (p=head;p!=NULL;p=p->next) printf("%d ",p->x);
+				printf("\ntot=%d\n",n);
 				break;
 			}
 			case 4:{
-				
+				m=read(); l=1;
+				for (q=head;q!=NULL;l++,q=q->next) if (q->x==m) break;
+				if (q==NULL) printf("-1\n"); else printf("%d\n",l);
 				break;
 			}
 			case 5:{
-				
+				if (q==NULL) {
+					printf("-1\n");
+					break;
+				}
+				for (q=q->next;q!=NULL;l++,q=q->next) if (q->x==m) break;
+				if (q==NULL) printf("-1\n"); else printf("%d\n",l);
 				break;
 			}
-			case 6:return 0;
+			case 6:{
+				for (p=head;p!=NULL;p=tail) tail=p->next,delete p;
+				head=tail=p=q=NULL;
+				return 0;
+			}
 		}
 	}
 	
