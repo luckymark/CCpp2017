d32e9e813d5c86db4eb3e41ee0eccb0d1b7a8ce6
= <=>
2017-03-01 12:45:25 +0800

    1

---
+++ practices/c/level1/p06_Goldbach/Godback.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+	int  t=1;
+	int c[101] = { 0 };
+	int prime[26];
+	for (int a = 2;a <= 10;a++){
+		if (c[a] == 0){
+			for (int b = 2 * a;b <= 100;b += a)
+				c[b] = 1;
+		}
+	}
+	for (int a = 2;a <= 100;a++){
+		if (c[a] == 0) {
+			prime[t] = a;
+			t += 1;
+		}
+	}
+	int m, n;
+	for (m = 1;m <= 50;m++){
+		n = 2 * m;
+		for (int p = 1;p <= 26;p++) {
+			for (int d = 1;d <= p;d++)
+				if (n == prime[p] + prime[d])
+					printf("%2d=%2d+%2d\t", n, prime[p], prime[d]);
+		}
+	}
+	printf("\n\n");
+	if (m = 50)printf("因此，在100以内哥德巴赫猜想正确。");
+	else printf("因此，在100以内哥德巴赫猜想不正确。");
+}
+
