e37ae13dbd5822315bb427a1b5838275bfe9bc13
xiaoyuann <602632139@qq.com>
2017-03-04 16:53:41 +0800

    level 0

---
+++ practices/c/level0/0-9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+  int number[10]={0};
+  int i,j;
+
+  for(i=1;i<=5;i++){
+    scanf("%d",&j);
+    number[j]=1;
+  }
+
+  for(i=1;i<=9;i++){
+   if(number[i]==0){
+    printf("%d",i);
+  }
+
+  }
+   return 0;
+}
+
