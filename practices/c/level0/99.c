94d44459fcefb25672a23697911f6b7a2e10e07a
xiaoyuann <602632139@qq.com>
2017-03-04 16:55:05 +0800

    Revert "Revert "level 0""
    This reverts commit 7df57746fe093b1f286f97a77b95bc515dade6c4.

---
+++ practices/c/level0/99.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+   int i,j;
+
+   for(i=1;i<=9;++i){
+     for(j=1;j<=i;++j)
+        printf("%d*%d=%d\t",j,i,i*j);
+        printf("\n");
+   }
+
+
+   return 0;
+}
+
