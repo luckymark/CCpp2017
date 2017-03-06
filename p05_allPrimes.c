e37f70428e77c46d6b14f422bd479fd568e978d1
Philosjay <305546044@qq.com>
2017-03-06 22:06:58 +0800

    Update p05_allPrimes.c

--- p05_allPrimes.c
+++ p05_allPrimes.c
@@ -1,25 +1,28 @@
 ﻿#include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-int isprinme(int a);
+int isPrinme(int a);
 int IS(int);
 int main()
 {
 	clock_t t1, t2;
 	t1 = clock();
+	
 	for(int i=2;i<=1000;i++)
 	{
-		if (isprinme(i))
+		if (isPrime(i))
 			printf("%5d", i);
 	}
 	t2 = clock();
 	printf("打印时间:%f秒",(double)(t2-t1)/CLOCKS_PER_SEC);
+	
 	system("pause");
 	return 0;
 }
-int isprinme(int a) 
+int isPrime(int a) 
 {
 	int mark = 0;
+	
 	for (int i = 2; i < a; i++)
 	{
 		if (a%i == 0)	//用mark标记a已被整除
