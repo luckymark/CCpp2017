5c6e09fa9b1e67a3637a8bb4bfc712ee5b5d7328
KilloCalorist <wh843147887@qq.com>
2017-03-06 20:18:28 +0800

    Update p01_runningLetter.cpp

--- homework2017.2.26/p01_runningLetter.cpp
+++ homework2017.2.26/p01_runningLetter.cpp
@@ -11,10 +11,18 @@ int main()
 		for (step=1;step<len_now;step++)
 			printf(" ");
 		printf("R");
-		if (goBack) len_now--;
-		else len_now++;
-		if (len_now==lenth)	goBack=1;
-		else if (len_now==0)	goBack=0;
+		if (goBack){
+			len_now--;
+		}		
+		else{
+			len_now++;
+		}		
+		if (len_now==lenth){
+			goBack=1;
+		}		
+		else if (len_now==0){
+			goBack=0;
+		}		
 		sleep(50);
 	}
 	return 0;
