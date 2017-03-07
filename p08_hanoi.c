5462f1776f23068720d328ad53b8800f539b13ef
Philosjay <305546044@qq.com>
2017-03-07 20:30:52 +0800

    Update p08_hanoi.c

--- p08_hanoi.c
+++ p08_hanoi.c
@@ -3,13 +3,13 @@
 #define WIDTH 11
 
 void hanoi(char a, char b, char c, int n);
-int step = 0;
+int n = 0;
 
 void main()
 {
 	freopen_s("out.txt", "w", stdout);
 	hanoi('A','B','C',WIDTH);
-	printf("%d steps", step);
+	printf("%d steps", n);
 	system("pause");
 }
 void hanoi(char a, char b, char c, int n)
@@ -17,11 +17,11 @@ void hanoi(char a, char b, char c, int n)
 	if (n == 1)
 	{
 		printf("move %d from %c to %c\n", n, a, c);
-		step++;
+		n++;
 		return;
 	}
 	hanoi(a, c, b, n - 1);
 	printf("move %d from %c to %c\n", n, a, c);
-	step++;
+	n++;
 	hanoi(b, a, c, n - 1);
 }
