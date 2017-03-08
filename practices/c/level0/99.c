13f7e95f1360257e4aa942e18abdd5e09e691bce
xiaoyuann <602632139@qq.com>
2017-03-08 23:00:32 +0800

    Update 99.c

--- practices/c/level0/99.c
+++ practices/c/level0/99.c
@@ -5,9 +5,9 @@ int main(void)
    int i,j;
 
    for(i=1;i<=9;++i){
-     for(j=1;j<=i;++j)
-        printf("%d*%d=%d\t",j,i,i*j);
-        printf("\n");
+      for(j=1;j<=i;++j)
+         printf("%d*%d=%d\t",j,i,i*j);
+         printf("\n");
    }
 
 
