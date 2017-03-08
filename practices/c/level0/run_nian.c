e5601979e6b48d33a8399eaaafd8ffdbac5706ee
xiaoyuann <602632139@qq.com>
2017-03-08 23:01:27 +0800

    Update run_nian.c

--- practices/c/level0/run_nian.c
+++ practices/c/level0/run_nian.c
@@ -9,9 +9,9 @@ int main(void)
 
 	if ((0 != year % 100 && 0 == year % 4) || 0 == year % 400)
 
-        printf("%d is leap year",year);
-    else
-        printf("%d 不是闰年",year);
+            printf("%d is leap year",year);
+        else
+            printf("%d 不是闰年",year);
 
 	return 0;
 }
