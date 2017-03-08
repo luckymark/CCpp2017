6c1c9dc74e80840561b9f2688209201b5201e97b
xiaoyuann <602632139@qq.com>
2017-03-08 22:59:02 +0800

    Update 3_5.c

--- practices/c/level0/3_5.c
+++ practices/c/level0/3_5.c
@@ -5,9 +5,9 @@ int main(void)
     int k,i=0;
 
     for(k =0 ;i <= 100;++i){
-     k = ++k;
-     if(0 == k % 3 && 0 != k % 5){
-        printf("%d\n",k);
+        k = ++k;
+        if(0 == k % 3 && 0 != k % 5){
+           printf("%d\n",k);
         }
      }
 
