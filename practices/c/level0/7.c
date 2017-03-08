6d26b48e4e2a8493265799076a18109670c0e83d
xiaoyuann <602632139@qq.com>
2017-03-08 22:59:57 +0800

    Update 7.c

--- practices/c/level0/7.c
+++ practices/c/level0/7.c
@@ -5,14 +5,14 @@ int main(void)
     int k,j=0,i=0;
 
     for(k =0 ;i < 100;++i){
-     k = ++k;
-     if(0 == k % 7 ){
-        printf("%d\n",k);
+        k = ++k;
+        if(0 == k % 7 ){
+           printf("%d\n",k);
         }
     }
 
      for( j = 0;j<10;++j){
-        printf("%d\n",k=j*10+7);
+         printf("%d\n",k=j*10+7);
         }
 
 
