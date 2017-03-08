3512fdede4bf6af6de2f30df6d316cfdaf993b8a
Knorringite <935018938@qq.com>
2017-03-08 23:04:15 +0800

    Correct

---
+++ practices/c/level1/p01_runningLetter/p01_Runningletter.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+ 
+int main()
+{
+    char c;
+    scanf("%c", &c);
+    system("mode con cols=16 lines=9");
+    int width = GetSystemMetrics ( SM_CXSCREEN );
+    for (int i = 0; i < 32; i++)
+    {
+        system("cls");
+        if(i<16)
+        {
+        	for(int j = 0; j < i; j++)
+        	{
+            	printf(" ");
+        	}
+        	printf("%c", c);
+        	Sleep(100);
+        }
+        else
+        {
+        	for (int j = 0; j < 31 - i; j++)
+    		{
+		    	printf (" ");
+			}
+			printf ("%c", c);
+			Sleep (100);
+			printf ("\n");
+		}
+    }
+    return 0;
+}
---
+++ practices/c/level1/p04_ narcissus/p04_Narcissus.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{
+	for(int i=100; i < 1000; i++)
+	{
+		if(pow (i / 100, 3) + pow((i % 100) / 10, 3) + pow(i % 10, 3) == i)
+		{
+			printf("%d\n", i);
+		}
+	}
+    return 0;
+}
