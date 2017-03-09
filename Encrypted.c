975dccd22150fe9373fa4877b8c2c7e35189827a
derderking <525655072@qq.com>
2017-03-09 20:14:48 +0800

    Homework 1-8 test

---
+++ Encrypted.c
@@ -0,0 +1,44 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h>
+void Encrypte(char* p)
+{
+	int i = 0;
+	while (p[i] != '\0')
+	{
+		p[i] = p[i] + '3' - '1';
+		printf("%c", p[i]);
+		i++;
+	}
+	printf("\n");
+}
+void Decrypte(char* p)
+{
+	int i = 0;
+	while (p[i] != '\0')
+	{
+		p[i] = p[i] - '3' + '1';
+		printf("%c", p[i]);
+		i++;
+	}
+	printf("\n");
+}
+int main()
+{
+	char p[10000];
+	int Judge = 0;
+	printf("Please input the string need opreating\n");
+	scanf("%s", p);
+	printf("En or De?\nPleaes input 1(En) or 2(De)\n");
+	scanf("%d", &Judge);
+	if (Judge == 1)
+	{
+		Encrypte(p);
+	}
+	else if (Judge == 2)
+	{
+		Decrypte(p);
+	}
+    return 0;
+}
+
