d1fe42dd204113cf558ad701d633bba959ebb215
Rainbowow <victorainbow123@gmail.com>
2017-03-09 13:43:11 +0800

    Update level_1_p05.c

--- practices/c/level1/p05_allPrimes/level_1_p05.c
+++ practices/c/level1/p05_allPrimes/level_1_p05.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
-#include<math.h>
 #include<time.h>
-int prime(int);
-main()
-{
-	int i,j;
-	for(i=2;i<=1000;i++){
-		if(prime(i)>0)
-		printf("%d\n",i);
-	}
-	printf("\n");
-	printf("%d",(int)clock());
-}
 
 int prime(int m)
-{
-	int k;int i;
-	k=sqrt(m);
-	for (i=2;i<=k;i++)
-		if (m%i==0) break;
-		if (i>k) return m;
-	else return 0;
+{	
+	for (int i=2;i*i<=m;i++){
+		if(m%i==0)
+		{return 0;}
+	}
+	return 1;
+}
+
+main()
+{		
+	double start，finish;
+	start=clock();
+	for(i=2;i<=1000;i++){
+		if(prime(i)>0){
+		printf("%d\n",i);
+		}
+	}
+	finish=clock();
+	printf("\n");
+	printf("%f",finish-start);
 }
