81ee9f7d055e7f29ad437cda226355ce5defd4fc
=Rain <=942996670@qq.com>
2017-03-10 23:49:06 +0800

    2017.03.10

---
+++ practices/c/level1/p07_encrypt_decrypt/encrypt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	char ch[80],tran[80];
+	printf("input cipher code:");
+	gets(ch);
+	printf("\ncopher code :%s",ch);
+	j=0;
+	if(j;ch[j]!='\0'; j++){
+		if((ch[j]>='A')&&(ch[j]<='Z')){
+		tran[j]=155-ch[j]; 
+	} else if((ch[j]>='a')&&(ch[j]<='z')){	
+	}
+}
+ 
