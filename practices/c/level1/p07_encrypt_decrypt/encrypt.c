b5bdf98f7a1ba3e5c2c90ec2f8958ac0b666a227
=Rain <=942996670@qq.com>
2017-03-11 01:07:56 +0800

    2017.03.11（2）

--- practices/c/level1/p07_encrypt_decrypt/encrypt.c
+++ practices/c/level1/p07_encrypt_decrypt/encrypt.c
@@ -1,12 +1,42 @@
+ 
 #include<stdio.h>
 int main()
 {
-	int i,j;
+
+	return 0;
+}
+
+
+int encrypt()
+{
+	int i,j=0;
 	char ch[80],tran[80];
 	printf("input cipher code:");
 	gets(ch);
 	printf("\ncopher code :%s",ch);
-	j=0;
+	if(j;ch[j]!='\0'; j++){
+		if((ch[j]>='A')&&(ch[j]<='Z')){
+			tran[j]=155-ch[j]; 
+		}else if((ch[j]>='a')&&(ch[j]<='z')){
+			tran[j]=219-ch[j];	
+		}else {
+			tran[i]=ch[j];		
+		}
+	n=j;
+	printf("\noriginal text:");
+	for(j=0;j<n;j++){
+		putchar(tran[j])
+	}
+	}
+}
+
+int decrypt()
+{
+	int i,j=0;
+	char ch[80],tran[80];
+	printf("input original code:");
+	gets(ch);
+	printf("\noriginal code :%s",ch);
 	if(ch[j];ch[j]!='\0'; j++){
 		if((ch[j]>='A')&&(ch[j]<='Z')){
 			tran[j]=155-ch[j]; 
@@ -15,11 +45,11 @@ int main()
 		}else {
 			tran[i]=ch[j];		
 		}
-	}
 	n=j;
-	printf("\noriginal text:");
+	printf("\ncipher text:");
 	for(j=0;j<n;j++){
 		putchar(tran[j])
 	}
-	return 0;
+	}
 }
+ 
