f4ce25ea10e2881cc479f8001ae68cccc6f07c7d
=Rain <=942996670@qq.com>
2017-03-11 00:45:27 +0800

    2017.03.11

--- practices/c/level1/p07_encrypt_decrypt/encrypt.c
+++ practices/c/level1/p07_encrypt_decrypt/encrypt.c
@@ -7,10 +7,19 @@ int main()
 	gets(ch);
 	printf("\ncopher code :%s",ch);
 	j=0;
-	if(j;ch[j]!='\0'; j++){
+	if(ch[j];ch[j]!='\0'; j++){
 		if((ch[j]>='A')&&(ch[j]<='Z')){
-		tran[j]=155-ch[j]; 
-	} else if((ch[j]>='a')&&(ch[j]<='z')){	
+			tran[j]=155-ch[j]; 
+		}else if((ch[j]>='a')&&(ch[j]<='z')){
+			tran[j]=219-ch[j];	
+		}else {
+			tran[i]=ch[j];		
+		}
 	}
+	n=j;
+	printf("\noriginal text:");
+	for(j=0;j<n;j++){
+		putchar(tran[j])
+	}
+	return 0;
 }
- 
