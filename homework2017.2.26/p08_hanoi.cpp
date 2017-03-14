eabb1a0621d8c06fdbd5706eea1a69492788d527
KilloCalorist <wh843147887@qq.com>
2017-03-14 13:30:05 +0800

    2017.3.14

---
+++ homework2017.2.26/p08_hanoi.cpp
@@ -0,0 +1,19 @@
+#include<cstdio>
+
+void hanoi(int n,char from,char mid,char aim);
+int main(){
+	int n;
+	scanf("%d",&n);
+	hanoi(n,'a','b','c');
+	return 0;
+}
+void hanoi(int n,char from,char mid,char aim){
+	if (n==1) {
+		printf("%c -> %c\n",from,aim);
+	}
+	else {
+		hanoi(n-1,from,aim,mid);
+		printf("%c -> %c\n",from,aim);
+		hanoi(n-1,mid,from,aim);
+	}
+}
