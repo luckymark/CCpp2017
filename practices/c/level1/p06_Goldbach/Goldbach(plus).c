c502e87b9b956b269646180cd6dd287f2bdfecc8
xiaoyuann <602632139@qq.com>
2017-03-23 19:36:12 +0800

    Update Goldbach(plus).c

--- practices/c/level1/p06_Goldbach/Goldbach(plus).c
+++ practices/c/level1/p06_Goldbach/Goldbach(plus).c
@@ -9,7 +9,7 @@ int pri[MAX]={0};
 
 int main(void){
     int n;
-    printf("please input n(4~%d之间的偶数):",MAX);
+    printf("please input n(4~%d脰庐录盲碌脛脜录脢媒):",MAX);
     scanf("%d",&n);
 
     if(n%2 == 0){
@@ -20,6 +20,7 @@ int main(void){
     else{
         printf("error input");
     }
+    
     return 0;
     }
 
@@ -31,9 +32,9 @@ int prime(int a){
     if(i=2){
             pri[i]=0;}
     for(i=3;i<=MAX;i++){
-        for(j=2;j<=sqrt(i);j++){
-            if(i%j==0){
-                pri[i]=1;
+         for(j=2;j<=sqrt(i);j++){
+              if(i%j==0){
+                  pri[i]=1;
             }
         }
     }
@@ -42,12 +43,11 @@ int prime(int a){
 int Goldbahe(int b){
     int i,j;
     for(i=1;i<=MAX;i++){
-            for(j=i;j<=MAX;j++){
-                if(pri[i]==0&pri[j]==0&b==i+j){
-                    printf("%d = %d + %d\n",b,i,j);
-    }
+         for(j=i;j<=MAX;j++){
+              if(pri[i]==0&pri[j]==0&b==i+j){
+                  printf("%d = %d + %d\n",b,i,j);
            }
-
         }
-    }
+     }
+ }
 
