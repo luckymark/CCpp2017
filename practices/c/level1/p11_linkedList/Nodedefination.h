e6ac33c46bed2b84ec024b4fbc1908f919943102
helloworld2019 <969507437@qq.com>
2017-03-23 18:45:39 +0800

    firstly use huge project way to code

---
+++ practices/c/level1/p11_linkedList/Nodedefination.h
@@ -0,0 +1,22 @@
+/*
+ * Nodedefination.h
+ *
+ *  Created on: 2017年3月22日
+ *      Author: mark
+ */
+
+#ifndef NODEDEFINATION_H_
+#define NODEDEFINATION_H_
+
+class Node
+{
+public:
+	int Data ;
+
+	Node * Next;
+
+};
+
+
+
+#endif /* NODEDEFINATION_H_ */
---
+++ practices/c/level1/p11_linkedList/inverted.cpp
@@ -0,0 +1,67 @@
+/*
+ * inverted.cpp
+ *
+ *  Created on: 2017年3月23日
+ *      Author: mark
+ */
+#include<iostream>   //quetion? when compile, this document cost memeory?
+#include"Nodedefination.h"
+
+using namespace std;
+
+Node* inverted(Node* start,int n)
+{
+
+	Node* p = start ;
+
+	Node* address[n];
+
+	int i=0;
+
+	while ( p != NULL ) {
+
+		address[i] = p ;
+
+		if(p->Next == NULL )break;
+
+		p = p->Next ;
+
+		i++;
+
+	}
+
+	Node* ret = p;
+
+	p = start ;
+
+	Node* temp =NULL;
+
+	int count=0;
+
+	while(p != NULL){
+
+		temp = p->Next;
+
+		if(address[count]==p){
+
+			p->Next = NULL ;
+
+		}
+
+		else{
+
+			p->Next = address[count] ;
+
+			count++;
+
+		}
+
+		p=temp;
+
+	}
+
+
+	return ret ;
+
+}
+
