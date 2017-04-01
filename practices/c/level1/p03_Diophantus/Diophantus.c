9736e55e8d45d2d79b74ae4ab9c07b85a73901d0
李帅锋 <hotwindli@hotmail.com>
2017-04-01 18:25:32 +0800

    Create Diophantus.c

---
+++ practices/c/level1/p03_Diophantus/Diophantus.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main(void)
+{
+	int age;
+	//  age/6+age/12+age/7+5 = age-age/2-4
+	
+	age = 28*9/3;
+	printf("when diophantus's child dead,diophantus was ：%d years old.",age-4);
+	
+	return  0;
+ } 
