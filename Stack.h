3211c9cac0534dc61ed943d37ad60ee799a31aec
Philosjay <305546044@qq.com>
2017-04-03 11:44:54 +0800

    Stack.cpp

---
+++ Stack.h
@@ -0,0 +1,20 @@
+﻿#ifndef STACK_H_
+#define STACK_H_
+
+#define n 100
+
+class Stack
+{
+private:
+	int item[n];
+	int pos ;
+public:
+	void push(int item);
+	void pop();
+	int IsFull();
+	int IsEmpty();
+	void show();
+	Stack();
+};
+
+#endif
