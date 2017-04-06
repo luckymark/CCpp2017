b5de4c8748116e152e5e92a06076f36303f0c968
AutSky-JadeK <1430568197@qq.com>
2017-04-06 11:44:42 +0800

    修改maze一题：不用注释备份代码

--- maze_test.c
+++ maze_test.c
@@ -208,19 +208,7 @@ int main(){
 		a[px[id2]][n+1]=' ';
 		aimx=px[id2];
 		aimy=n+1;
-	}/*
-	
-	for(int i=0;i<=n+1;++i){
-		for(int j=0;j<=n+1;++j){
-			putchar(a[i][j]);
-		}
-		puts("");
 	}
-	
-	puts("");
-	puts("");
-	puts("");
-	*/
 	
 	for(int i=1;i<=20;++i){
 		for(int j=1;j<=20;++j){
