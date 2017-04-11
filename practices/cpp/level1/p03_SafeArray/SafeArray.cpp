300a141ee455e543c82648a32df5b89b46de020a
winterYANGWT <q916090796@gmail.com>
2017-04-11 21:16:28 +0800

    finished safearray

---
+++ practices/cpp/level1/p03_SafeArray/SafeArray.cpp
@@ -0,0 +1,60 @@
+#include "SafeArray.h"
+
+safearray::safearray(void)
+{
+	num = 0;
+	arr = NULL;
+}
+
+safearray::safearray(int n)
+{
+	num = n;
+	arr = new int[n];
+	for(int i=0;i<n;++i)
+	{
+		arr[i] = 0;
+	}
+}
+
+safearray::safearray(const safearray & a)
+{
+	num = a.num;
+	arr = new int[num];
+	for (int i = 0; i < num; ++i)
+	{
+		arr[i] = a.arr[i];
+	}
+}
+
+safearray::~safearray(void)
+{
+	delete[]arr;
+}
+
+int& safearray::operator[](int n)
+{
+	if (n>=num) 
+	{
+		std::cout << "error" << std::endl;
+		int a;
+		return a;
+	}
+	else
+	{
+		return arr[n];
+	}
+		
+}
+
+safearray & safearray::operator=(const safearray & a)
+{
+	num = a.num;
+	arr = new int[num];
+	for (int i = 0; i < num; ++i)
+	{
+		arr[i] = a.arr[i];
+	}
+	return *this;
+	// TODO: insert return statement here
+}
+
