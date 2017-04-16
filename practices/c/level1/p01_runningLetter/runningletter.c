d504c5ed743876b7b29ad8fa7a4a1614fbac6d7b
helloworld2019 <969507437@qq.com>
2017-04-16 10:37:53 +0800

    delete space

--- practices/c/level1/p01_runningLetter/runningletter.c
+++ practices/c/level1/p01_runningLetter/runningletter.c
@@ -9,14 +9,14 @@ int main(void)
 	int i , j ;
 	char b[5][5];
 
-	for( i = 0 ; i<5 ; i++ ){
+    for( i = 0 ; i<5 ; i++ ){
 
         for( j=0 ; j<5 ; j++ ){
 			b[i][j] = ' ' ;
 		}
 	}
 
-	for(i = 0 ; i < 5 ; i++) {
+    for(i = 0 ; i < 5 ; i++) {
 
         for( j = 0 ; j < 5; j++) {
 			if(i != 0 && j== 0) {
@@ -29,11 +29,14 @@ int main(void)
 
 			int k ,f;
 
-			for( k = 0 ; k<= i ; k++ ){
+        for( k = 0 ; k<= i ; k++ ){
 
                 for( f=0 ; f<=j ; f++ ){
-					if( k != 0 && f == 0){ printf("\n"); }
-					printf("%c", b[k][f]);
+					if(k != 0 && f == 0){
+                        printf("\n"); 
+                    }
+                                        
+                    printf("%c", b[k][f]);
 				}
 			}
 		}
