af13ccf6d4dc33b2ee0dcc3a2a0e93529cbf8b0f
rabiribi <814017375@qq.com>
2017-04-24 15:51:02 +0800

    Create C3_5.c

---
+++ practices/c/level0/C3_5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>  
+
+int judgetri(int a,int b,int c)  
+    {
+        if ((a*a+b*b== c*c) || (a*a+c*c == b*b) || (b*b+c*c == a*a))
+    	  {
+    	      printf("该三角形为直角三角形");
+		    }
+		    else
+		    {
+			      printf("该三角形非直角三角形");
+		    } 
+    }  
+    
+int main()  
+    {
+        judgetri(3,4,5);
+    	
+    	  return 0;
+    }  
