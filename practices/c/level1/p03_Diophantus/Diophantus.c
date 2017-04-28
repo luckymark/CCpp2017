2fa10342095590a02e608cca5bf724c0b47a530c
李帅锋 <hotwindli@hotmail.com>
2017-04-28 13:03:15 +0800

    Updata procedure

--- practices/c/level1/p03_Diophantus/Diophantus.c
+++ practices/c/level1/p03_Diophantus/Diophantus.c
@@ -1,12 +1,13 @@
 #include <stdio.h> 
 
 int main(void)
-{
-	int age;
-	//  age/6+age/12+age/7+5 = age-age/2-4
-	
-	age = 28*9/3;
-	printf("when diophantus's child dead,diophantus was ：%d years old.",age-4);
-	
+{	
+	//  age/6+age/12+age/7+5 = age-age/2-4	
+	for(int i=10;i<=120;i++){
+        	if(i%6 == 0 && i%12 == 0 && i%7 == 0){
+            		printf("His age is %d",i);
+       	 	}
+    	}
+
 	return  0;
  } 
