f4952c5545286fb94983fbc63e8b10d9d81abcaf
rabiribi <814017375@qq.com>
2017-05-03 19:43:54 +0800

    Update C3_5.c

--- practices/c/level0/C3_5.c
+++ practices/c/level0/C3_5.c
@@ -1,20 +1,35 @@
-#include <stdio.h>  
+#include <stdio.h>
 
-int judgetri(int a,int b,int c)  
+struct Rect
+{
+	int left;
+	int right;
+	int top;
+	int bottom;
+}A,B;
+
+void judge(Rect A, Rect B)
     {
-        if ((a*a+b*b== c*c) || (a*a+c*c == b*b) || (b*b+c*c == a*a))
-    	  {
-    	      printf("该三角形为直角三角形");
-		    }
-		    else
-		    {
-			      printf("该三角形非直角三角形");
-		    } 
-    }  
-    
-int main()  
-    {
-        judgetri(3,4,5);
-    	
-    	  return 0;
-    }  
+        if(A.right <= B.left) printf("不重叠");
+        if(A.left >= B.right ) printf("不重叠");
+        if(A.top <= B.bottom) printf("不重叠");
+        if(A.bottom >= B.top) printf("不重叠");
+        else printf("重叠");
+	
+	return;
+    }
+
+int main()
+{
+	A.left=0;
+	A.right=1;
+	A.top=1;
+	A.bottom=0;
+	B.left=0;
+	B.right=2;
+	B.top=2;
+	B.bottom=0;
+	judge(Rect A, Rect B);
+	
+	return 0; 
+} 
