195d663d44898d4118adbaeab8d4a7b376aa320d
csw981001 <chenshuwei1001@icloud.com>
2017-05-31 09:35:17 +0800

    Update and rename practices/ cpp/ level1/ p11_Fighters/fighter to  practices/ cpp/ level1/ p11_Fighters/Fighter.cpp

--- practices/ cpp/ level1/ p11_Fighters/Fighter.cpp
+++ practices/ cpp/ level1/ p11_Fighters/Fighter.cpp
@@ -3,9 +3,10 @@
 #include<iostream>
 #include<windows.h>
 #include<time.h>
-#define move_speed_my_aircraft 0.5
-#define move_speed_my_missle 0.3
-#define move_speed_enemy_aircraft 0.15
+#define move_speed_my_aircraft 5
+#define move_speed_my_missle 3
+#define move_speed_enemy_aircraft 1.5
+#define move_speed_enemy_missile 2
 #define window_lenth 1920
 #define window_width 1020
 
@@ -37,7 +38,7 @@ public:
     sf::Texture tex_missile;
     sf::Sprite missile;
     };
-
+//我方战机类
 class MY_AIRCRAFT:public AIRCRAFT
 {
 public:
@@ -50,17 +51,9 @@ aircraft.setTexture(tex_aircraft);
 aircraft.setOrigin(106.5,150);
 aircraft.setPosition(960,930);
 aircraft.setScale(0.5f,0.5f);
-
-
-
     }
-
-
-sf::FloatRect boundingBox= aircraft.getGlobalBounds();
-
-
 };
-
+//我方导弹类
 class MY_MISSILE:public MISSILE
 {
 public:
@@ -75,10 +68,11 @@ missile.setOrigin(62.5,62.5);
 missile.setScale(0.3f,0.3f);
     }
 };
-
+//敌机类
 class ENEMY_AIRCRAFT:public AIRCRAFT
 {
 public:
+    int index=0;
     int if_exist=0;
     ENEMY_AIRCRAFT()
     {
@@ -90,6 +84,23 @@ aircraft.setOrigin(147,134);
 aircraft.setScale(0.4f,0.4f);
     }
 };
+//敌机导弹类
+class ENEMY_MISSILE:public MISSILE
+{
+public:
+    int if_begin_move=0;
+    int index=0;
+    int if_exist=0;
+    ENEMY_MISSILE()
+    {
+        if (!tex_missile.loadFromFile("missile.jpg")){
+    // error...
+}
+missile.setTexture(tex_missile);
+missile.setOrigin(62.5,62.5);
+missile.setScale(0.3f,0.3f);
+    }
+};
 int main()
 {
     //播放背景音乐
@@ -97,20 +108,23 @@ int main()
     if (!music.openFromFile("Sight.wav")){
     return -1;
     }
-    music.setVolume(30);
+    music.setVolume(10);
     music.play();
     music.setLoop(true);
     sf::Clock clock1;
     MY_MISSILE my_missile[50];
     ENEMY_AIRCRAFT enemy_aircraft[30];
-    int between_time,enemy_number,enemy_index;
+    ENEMY_MISSILE enemy_missile[30];
+    int between_time,enemy_number;
     int if_my_aircraft_exist;
     int your_life=3;
+    int your_blood_number;
     sf::RenderWindow window(sf::VideoMode(window_lenth,window_width), "SFML works!");
     clock_t start_time,finish_time;
     start_time=clock();
     srand(time(0));
     while(your_life>0){
+            your_blood_number=300;
     MY_AIRCRAFT *my_aircraft=new (MY_AIRCRAFT);
     if_my_aircraft_exist=1;
     while (window.isOpen()){
@@ -121,15 +135,17 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        finish_time=clock();
+    finish_time=clock();
     between_time=finish_time-start_time;
-    if(between_time>3000){
+    if(between_time>6000){
        enemy_number=rand()%3+1;
             for(int j=0;j<30;j++){
-                if(0==enemy_aircraft[j].if_exist){
+                if((0==enemy_aircraft[j].if_exist)&&(0==enemy_missile[j].if_exist)){
                 enemy_aircraft[j].if_exist=1;
-                enemy_index=rand()%window_lenth+1;
-                enemy_aircraft[j].aircraft.setPosition(enemy_index,80);
+                enemy_aircraft[j].index=rand()%window_lenth+1;
+                enemy_aircraft[j].aircraft.setPosition(enemy_aircraft[j].index,0);
+                enemy_missile[j].index=(rand()%window_width+1)/2;
+                enemy_missile[j].missile.setPosition(enemy_aircraft[j].index,enemy_missile[j].index);
                 enemy_number--;
                 if(0==enemy_number){
                     break;
@@ -152,7 +168,7 @@ int main()
             }
         }
     }
-        for(int i=0;i<50;i++){
+    for(int i=0;i<50;i++){
             if(1==my_missile[i].if_exist){
                 my_missile[i].missile.move(0,-move_speed_my_missle);
                 }
@@ -160,36 +176,68 @@ int main()
                 my_missile[i].if_exist=0;
                 }
             }
-            for(int j=0;j<30;j++){
-                if(1==enemy_aircraft[j].if_exist){
-                    enemy_aircraft[j].aircraft.move(0,move_speed_enemy_aircraft);
-                }
+    for(int j=0;j<30;j++){
+        if(enemy_aircraft[j].if_exist==1){
+            if((enemy_aircraft[j].aircraft.getPosition().y)>=(enemy_missile[j].index)){
+                enemy_missile[j].if_begin_move=1;
+            }
+        }
+            if((1==enemy_missile[j].if_begin_move)&&(enemy_missile[j].missile.getPosition().y<window_width)){
+                enemy_missile[j].if_exist=1;
+                enemy_missile[j].missile.move(0,move_speed_enemy_missile);
+        }
+            if(1==enemy_aircraft[j].if_exist){
+                enemy_aircraft[j].aircraft.move(0,move_speed_enemy_aircraft);
+        }
+            if(enemy_aircraft[j].if_exist==1){
                 if(enemy_aircraft[j].aircraft.getPosition().y>window_width){
                     enemy_aircraft[j].if_exist=0;
-                }
             }
+        }
+            if(1==enemy_missile[j].if_begin_move){
+                if(enemy_missile[j].missile.getPosition().y>=window_width){
+                    enemy_missile[j].if_exist=0;
+                    enemy_missile[j].if_begin_move=0;
+            }
+        }
+}
     window.clear(sf::Color::White);
     window.draw(my_aircraft->aircraft);
     for(int i=0;i<50;i++){
         if(1==my_missile[i].if_exist){
-
             window.draw(my_missile[i].missile);
         }
     }
     for(int j=0;j<30;j++){
         if(1==enemy_aircraft[j].if_exist){
-
             window.draw(enemy_aircraft[j].aircraft);
         }
+        if(1==enemy_missile[j].if_exist){
+            window.draw(enemy_missile[j].missile);
+        }
     }
+
+
+
+    sf::VertexArray quad(sf::Quads,4);
+    quad[0].position=sf::Vector2f(10,10);
+    quad[1].position=sf::Vector2f(10+your_blood_number,10);
+    quad[2].position=sf::Vector2f(10+your_blood_number,60);
+    quad[3].position=sf::Vector2f(10,60);
+    quad[0].color=sf::Color::Red;
+    quad[1].color=sf::Color::Red;
+    quad[2].color=sf::Color::Red;
+    quad[3].color=sf::Color::Red;
+    window.draw(quad);
+
     for(int i=0;i<50;i++){
         for(int j=0;j<30;j++){
             if((1==my_missile[i].if_exist)&&(1==enemy_aircraft[j].if_exist)){
-                    sf::FloatRect boundingBox1 = enemy_aircraft[j].aircraft.getGlobalBounds();
+                sf::FloatRect boundingBox1 = enemy_aircraft[j].aircraft.getGlobalBounds();
                 sf::FloatRect boundingBox2 = my_missile[i].missile.getGlobalBounds();
                 if(boundingBox1.intersects(boundingBox2)){
-                        my_missile[i].if_exist=0;
-                enemy_aircraft[j].if_exist=0;
+                    my_missile[i].if_exist=0;
+                    enemy_aircraft[j].if_exist=0;
                 }
             }
         }
@@ -197,15 +245,37 @@ int main()
     sf::FloatRect boundingBox3 = my_aircraft->aircraft.getGlobalBounds();
     for(int j=0;j<30;j++){
         if(1==enemy_aircraft[j].if_exist){
-                sf::FloatRect boundingBox1 = enemy_aircraft[j].aircraft.getGlobalBounds();
+            sf::FloatRect boundingBox1 = enemy_aircraft[j].aircraft.getGlobalBounds();
             if(boundingBox3.intersects(boundingBox1)){
-                    if_my_aircraft_exist=0;
-                    your_life--;
+
+                enemy_aircraft[j].if_exist=0;
+                your_blood_number-=20;
                 break;
             }
         }
+
     }
+            for(int j=0;j<30;j++){
+                if(1==enemy_missile[j].if_exist){
+            sf::FloatRect boundingBox2 = enemy_missile[j].missile.getGlobalBounds();
+            if(boundingBox2.intersects(boundingBox3)){
+            enemy_missile[j].if_exist=0;
+            enemy_missile[j].if_begin_move=0;
+            enemy_missile[j].index=window_lenth;
+                your_blood_number-=10;
+                break;
+            }
+        }
+            }
+            if(your_blood_number<=0){
+                if_my_aircraft_exist=0;
+            }
     if(0==if_my_aircraft_exist){
+            for(int j=0;j<30;j++){
+                enemy_aircraft[j].if_exist=0;
+                enemy_missile[j].if_exist=0;
+                enemy_missile[j].if_begin_move=0;
+            }
       window.clear(sf::Color::White);
       window.display();
       Sleep(3000);
