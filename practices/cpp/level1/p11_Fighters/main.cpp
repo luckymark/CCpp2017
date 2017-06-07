ca79aa495e98ec948fd40ff6a6ce58b404a6611a
guyaqi5858 <1360668777@qq.com>
2017-06-07 15:57:07 +0800

    bgm added

--- practices/cpp/level1/p11_Fighters/main.cpp
+++ practices/cpp/level1/p11_Fighters/main.cpp
@@ -1,14 +1,22 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(540, 720), "SFML works!");
+
+    //load hero
     sf::Texture t_hero;
     t_hero.loadFromFile("assets/hero.png");
     sf::Sprite hero;
     hero.setTexture(t_hero);
     hero.setScale(0.5,0.5); //PLANE--------(128,128)*0.5=(64,64)
     hero.setPosition(540*0.5-32,720-64);
+
+    //load bgm
+    sf::Music bgm;
+    bgm.openFromFile("assets/bgm1.ogg");
+    bgm.play();
 
     while (window.isOpen())
     {
