54ef8a8dbd6a56e71b9a11f374564276e5053cd0
guyaqi5858 <1360668777@qq.com>
2017-06-07 17:51:31 +0800

    add fire

--- practices/cpp/level1/p11_Fighters/main.cpp
+++ practices/cpp/level1/p11_Fighters/main.cpp
@@ -1,23 +1,32 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Window.hpp>
+#include <vector>
 
 const int STAGE_X=540;
 const int STAGE_Y=720;
 const int PLANE_L=64;
+const int FIRE_L=32;
 
 
 void handlePlayerInput(sf::Keyboard::Key, bool);
 void update();
+void fire();
 
 //movement params
 bool isMovingUp=false;
 bool isMovingDown=false;
 bool isMovingLeft=false;
 bool isMovingRight=false;
+bool FiringKey=false;
+
 
 sf::Texture t_hero;
 sf::Sprite hero;
+sf::Texture t_herofire;
+sf::Sprite herofire;
+
+std::vector<sf::Vector2f> herofirevector;
 
 int main()
 {
@@ -28,6 +37,10 @@ int main()
     hero.setTexture(t_hero);
     hero.setScale(0.5,0.5); //PLANE--------(128,128)*0.5=(64,64)
     hero.setPosition(540*0.5-32,720-64);
+
+    //load fire
+    t_herofire.loadFromFile("assets/herofire.png");
+    herofire.setTexture(t_herofire);
 
     //load bgm
     sf::Music bgm;
@@ -58,9 +71,14 @@ int main()
 
         update();
         
-
+        //render
         window.clear();
         window.draw(hero);
+        for(std::vector<sf::Vector2f>::iterator it=herofirevector.begin();it!=herofirevector.end();it++)
+        {
+            herofire.setPosition(*(it));
+            window.draw(herofire);
+        }
         window.display();
     }
 
@@ -77,18 +95,47 @@ void handlePlayerInput(sf::Keyboard::Key key,bool isPressed)
         isMovingLeft = isPressed;
     else if (key == sf::Keyboard::D)
         isMovingRight = isPressed;
+    else if (key == sf::Keyboard::Space)
+    {
+        if(isPressed&&!FiringKey)
+        {
+            FiringKey=1;
+            fire();
+        }
+        else if(!isPressed&&FiringKey)
+        {
+            FiringKey=0;
+        }
+    }
 }
 
 void update()
 {
+    //hero move
     sf::Vector2f movement(0.f, 0.f);
     if (isMovingUp&&hero.getPosition().y>0)
-        movement.y -= 0.4f;
+        movement.y -= 0.15f;
     if (isMovingDown&&hero.getPosition().y<STAGE_Y-PLANE_L)
-        movement.y += 0.4f;
+        movement.y += 0.15f;
     if (isMovingLeft&&hero.getPosition().x>0)
-        movement.x -= 0.4f;
+        movement.x -= 0.15f;
     if (isMovingRight&&hero.getPosition().x<STAGE_X-PLANE_L)
-        movement.x += 0.4f;
+        movement.x += 0.15f;
     hero.move(movement);
+
+    //herofire move
+    for(std::vector<sf::Vector2f>::iterator it=herofirevector.begin();it!=herofirevector.end();it++)
+    {
+        sf::Vector2f movement(0,-0.2f);
+        *(it)+=movement;
+    }
+}
+
+void fire()
+{
+    sf::Vector2f newfire;
+    newfire=hero.getPosition();
+    newfire.x+=PLANE_L-FIRE_L;
+    newfire.y-=FIRE_L;
+    herofirevector.push_back(newfire);
 }
