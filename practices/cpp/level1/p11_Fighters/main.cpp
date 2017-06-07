028074bf07fa9966e0f7697dad470fef037d626d
guyaqi5858 <1360668777@qq.com>
2017-06-08 03:59:34 +0800

    add enemy

--- practices/cpp/level1/p11_Fighters/main.cpp
+++ practices/cpp/level1/p11_Fighters/main.cpp
@@ -3,6 +3,12 @@
 #include <SFML/Window.hpp>
 #include <vector>
 
+//for debug
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
 const int STAGE_X=540;
 const int STAGE_Y=720;
 const int PLANE_L=64;
@@ -12,6 +18,7 @@ const int FIRE_L=32;
 void handlePlayerInput(sf::Keyboard::Key, bool);
 void update();
 void fire();
+void spawnEnemy();
 
 //movement params 
 bool isMovingUp=false;
@@ -25,8 +32,11 @@ sf::Texture t_hero;
 sf::Sprite hero;
 sf::Texture t_herofire;
 sf::Sprite herofire;
+sf::Texture t_enemy;
+sf::Sprite enemy;
 
 std::vector<sf::Vector2f> herofirevector;
+std::vector<sf::Vector2f>enemyvector;
 
 int main()
 {
@@ -42,12 +52,20 @@ int main()
     t_herofire.loadFromFile("assets/herofire.png");
     herofire.setTexture(t_herofire);
 
+    //load enemy
+    t_enemy.loadFromFile("assets/enemy.png");
+    enemy.setTexture(t_enemy);
+    enemy.setScale(0.5,0.5);
+    enemy.setRotation(180);
+
     //load bgm
     sf::Music bgm;
     bgm.openFromFile("assets/bgm1.ogg");
     bgm.play();
 
-    
+    //set main clock
+    sf::Clock spawnclock;
+    float interval=0;
 
     while (window.isOpen())
     {
@@ -69,6 +87,14 @@ int main()
             }
         }
 
+        //spawn enemy
+        if(spawnclock.getElapsedTime().asSeconds()>interval)
+        {
+            spawnEnemy();
+            srand(time(0));
+            interval=2+rand()%2;
+            spawnclock.restart();
+        }
         update();
         
         //render
@@ -78,6 +104,11 @@ int main()
         {
             herofire.setPosition(*(it));
             window.draw(herofire);
+        }
+        for(std::vector<sf::Vector2f>::iterator it=enemyvector.begin();it!=enemyvector.end();it++)
+        {
+            enemy.setPosition(*(it));
+            window.draw(enemy);
         }
         window.display();
     }
@@ -139,13 +170,27 @@ void update()
         }
        
     }
+    //enemy move
+    for(std::vector<sf::Vector2f>::iterator it=enemyvector.begin();it!=enemyvector.end();it++)
+    {
+        sf::Vector2f movement(0,0.1f);
+        *(it)+=movement;
+    }
 }
 
 void fire()
 {
     sf::Vector2f newfire;
     newfire=hero.getPosition();
-    newfire.x+=PLANE_L-FIRE_L;
+    newfire.x+=PLANE_L/2-FIRE_L/2;
     newfire.y-=FIRE_L;
     herofirevector.push_back(newfire);
+}
+
+void spawnEnemy()
+{
+    srand(time(0));
+    int x=(int)(rand()%(STAGE_X-PLANE_L));
+    sf::Vector2f newenemy(x,0);
+    enemyvector.push_back(newenemy);
 }
